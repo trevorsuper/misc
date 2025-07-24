b99e20f71eb45c9647a571cdd6bced924c005dc7
TrevorSuper <129561006+TrevorSuper@users.noreply.github.com>
2025-07-24 12:25:50 -0400

    add apt autoremove -y

--- pi5.sh
+++ pi5.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/sh
 sudo apt remove firefox chromium orca thonny geany dillo lynx emacsen-common -y
+sudo apt autoremove -y
 sudo apt update && sudo apt upgrade -y
 sudo apt install neofetch
 sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
